395d0544e6659f29c5d727e1b2e9650c76db4ff3
Holmes Yesid Ayala Yaguara <holmes.ayala@muncher.com.co>
2022-07-28 17:24:50 -0500

    Add favorite button to pokemon item

--- src/pages/Home/components/PokemonItem/PokemonItem.tsx
+++ src/pages/Home/components/PokemonItem/PokemonItem.tsx
@@ -3,16 +3,39 @@ import ListItemButton from '@mui/material/ListItemButton';
 import Avatar from '@mui/material/Avatar';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
+
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import Favorite from '@mui/icons-material/Favorite';
 
 export type PokemonItemProps = {
+	isFavorite?: boolean;
 	imageUrl: string;
 	name: string;
 	onClick?: () => void;
+	onChangeFavorite?: (newValue: boolean) => void;
 };
 
-function PokemonItem({ imageUrl, name, onClick }: PokemonItemProps) {
+function PokemonItem({
+	isFavorite = false,
+	imageUrl,
+	name,
+	onClick,
+	onChangeFavorite,
+}: PokemonItemProps) {
+	const onClickFromFavoriteButton = () => {
+		if (onChangeFavorite) onChangeFavorite(!isFavorite);
+	};
+
 	return (
-		<ListItem disablePadding>
+		<ListItem
+			disablePadding
+			secondaryAction={
+				<IconButton onClick={onClickFromFavoriteButton} color={isFavorite ? 'primary' : 'default'}>
+					{isFavorite ? <Favorite /> : <FavoriteBorder />}
+				</IconButton>
+			}
+		>
 			<ListItemButton onClick={onClick}>
 				<ListItemAvatar>
 					<Avatar src={imageUrl} alt={name} />
